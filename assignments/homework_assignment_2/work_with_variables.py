317f15ed74c2f150d3901ab9709b90c995ddfee7
soda00000 <60484800+soda00000@users.noreply.github.com>
2020-02-06 20:39:48 -0600

    Create work_with_variables.py

---
+++ assignments/homework_assignment_2/work_with_variables.py
@@ -0,0 +1,11 @@
+#Fu An Yang
+#1051888
+#Assignment#2
+#2/9
+Stock="AAPL"
+Price=204.66
+Changed=-4.08
+Yesterday=Price+Changed
+print("Symbol: "+Stock+", Price: "+str(Price)+", Change:"+str(Changed))
+print("Symbol: "+Stock.lower()+", Price: $"+str(Price)+", Change:"+str(Changed))
+print("Symbol: "+Stock+"---Yesterday's Price: "+str(Yesterday))
