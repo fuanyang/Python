7120f416d824a009332624e8fca0e054e76e7488
soda00000 <60484800+soda00000@users.noreply.github.com>
2020-02-14 00:38:02 -0600

    Create work_with_lists.py

---
+++ assignments/homework_assignment_3/work_with_lists.py
@@ -0,0 +1,17 @@
+#Fu An Yang
+#1051888
+#Assignment#2
+#2/16
+grocery=["banana","milk","egg","bread","shampoo"]
+price=[2,7,5,4,10]
+print(grocery[3]," $",price[3])
+print(grocery[-1]," $",price[-1])
+grocery.append("potato chips")
+price.append(2.99)
+print(grocery)
+print(price)
+grocery.pop(0)
+price.pop(0)
+price[1]=2*price[1]
+print(grocery)
+print(price)
