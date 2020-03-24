0f763c3851583c45af37434a40ddb95777ec43dc
soda00000 <60484800+soda00000@users.noreply.github.com>
2020-03-24 01:02:05 -0500

    Create is_it_in_stock.py

---
+++ assignments/homework_assignment_5/is_it_in_stock.py
@@ -0,0 +1,11 @@
+menu_items_in_stock=["Tacos","Chips","Salsa","Quesadilla"]
+Customer_order=["Tacos","Gucamole","Burrito","Chips","Salsa"]
+new_menu=[menu.lower() for menu in menu_items_in_stock]
+new_customer=[customer.lower() for customer in Customer_order]
+print(new_menu)
+print(new_customer)
+for order in Customer_order:
+    if order in menu_items_in_stock:
+        print("We have "+order+" in stock.")
+    else:
+        print("We do not have "+order+" in stock.")
