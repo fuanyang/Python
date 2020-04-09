10c5f2e39d9c9135b81a7465c9270fd29e0c839f
soda00000 <60484800+soda00000@users.noreply.github.com>
2020-04-09 13:40:30 -0500

    Update user_order.py

--- assignments/homework_assignment_7/user_order.py
+++ assignments/homework_assignment_7/user_order.py
@@ -5,7 +5,7 @@
 KFC={"burger":10,"fries":1,"ice_cream":2,"fry_chicken":7,"egg_tart":5}
 order_cost=[]
 food =KFC.keys()
-
+process_active = True
 while process_active:
     order=input("enter an item")
     if order=='N' :
