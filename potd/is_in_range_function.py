e72956f5a3edd69055d3a6804de3577feef25233
soda00000 <60484800+soda00000@users.noreply.github.com>
2020-04-16 01:50:25 -0500

    Create is_in_range_function.py

---
+++ potd/is_in_range_function.py
@@ -0,0 +1,11 @@
+#Fu An Yang
+#1051888
+#PotD#8
+#4/19
+def is_in_range_function(check_number,mini,maxm):
+    """check if the number is in the range"""
+    if check_number <= maxm and check_number >= mini:
+        print("True")
+    else:
+        print("False")
+  
